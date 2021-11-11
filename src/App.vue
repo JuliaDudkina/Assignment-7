1b692d6bdf4917296669a697c2adf529f105d58f
juliadud <yuliiadudkina@gmail.com>
2021-11-11 12:33:31 +0200

    outputing data with composition API

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <h2>My Course Goal</h2>
-  <!-- Task 1: Output your main course goal with help of the composition API -->
-  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h2>{{ user.name }}'s Main Goal is to</h2>
+  <h3>{{ user.mainGoal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
   <button>Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
@@ -13,8 +11,16 @@
 </template>
 
 <script>
+import {reactive} from 'vue';
 export default {
-  
+  setup(){
+    const user = reactive({
+      name: 'Julia',
+      mainGoal: 'learn Vue.js framework'
+    })
+
+    return{ user: user};
+  }
 }
 </script>
 
